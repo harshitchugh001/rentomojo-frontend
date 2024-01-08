59b2005c63e176b64a8eb42a28074463b552b500
Harshit <123458021+harshitchugh001@users.noreply.github.com>
2024-01-08 15:19:31 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   const openRegisterModal = () => {
     setIsRegisterOpen(true);
-  }
+  };
 
   const closeRegisterModal = () => {
     setIsRegisterOpen(false);
